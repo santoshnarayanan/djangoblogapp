70ddfe5cb27bf9980b3eda25d2dd67455794e5c8
santosh <santosh33@gmail.com>
2024-05-14 21:48:12 +0200

    Defined Post Model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.utils import timezone
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now) #publish  used python datetime now
+    created = models.DateTimeField(auto_now_add=True) # created field
+    updated = models.DateTimeField(auto_now=True) # updated field
+    #The sorting is defined in a class - Meta to sort by publish field
+    class Meta:
+        ordering = ['-publish']
+    def __str__(self):
+        return self.title
+    
