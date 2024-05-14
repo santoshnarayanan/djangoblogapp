995b2cc04e45da52e86bf135ac6ecf64f1c114bf
santosh <santosh33@gmail.com>
2024-05-14 22:22:49 +0200

    Post Model is completed

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,5 @@
-from django.db import models
-
 # Create your models here.
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
@@ -10,11 +9,17 @@ class Post(models.Model):
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    # Adding a many to  one relationship (user <---> posts)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, 
+                               on_delete=models.CASCADE, 
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now) #publish  used python datetime now
     created = models.DateTimeField(auto_now_add=True) # created field
     updated = models.DateTimeField(auto_now=True) # updated field
-    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT) #status field
+    status = models.CharField(max_length=2, 
+                              choices=Status, 
+                              default=Status.DRAFT) #status field
     #The sorting is defined in a class - Meta to sort by publish field
     class Meta:
         ordering = ['-publish']
