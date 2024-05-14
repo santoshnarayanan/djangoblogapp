c9e885be3fd01622c8ebf9b8f45d180f2f544786
santosh <santosh33@gmail.com>
2024-05-14 21:58:37 +0200

    Adding Status field

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -5,12 +5,16 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now) #publish  used python datetime now
     created = models.DateTimeField(auto_now_add=True) # created field
     updated = models.DateTimeField(auto_now=True) # updated field
+    status = models.CharField(max_length=2, choices=Status, default=Status.DRAFT) #status field
     #The sorting is defined in a class - Meta to sort by publish field
     class Meta:
         ordering = ['-publish']
