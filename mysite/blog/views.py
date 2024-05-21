fe92a95dbf1a0f38f037445cd92504e0f45d5fb2
santosh narayanan <santosh33@gmail.com>
2024-05-21 18:10:28 +0200

    Class based views for handling pagination

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,24 +1,14 @@
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 from django.shortcuts import get_object_or_404, render
 from django.http import Http404
 from .models import Post
 
-
-def post_list(request):
-    post_list = Post.published.all()
-    # Pagination with 3 Post per page
-    paginator = Paginator(post_list, 3)
-    page_number = request.GET.get("page", 1)
-
-    try:
-        posts = paginator.page(page_number)
-    except EmptyPage:
-        # if page_number is out of range get last page of results
-        posts = paginator.page(paginator.num_pages)
-        # if page is not an integer deliver the first page
-    except PageNotAnInteger:
-        posts = paginator.page(1)
-    return render(request, "blog/post/list.html", {"posts": posts})
+class PostListView(ListView):
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/post/list.html"
 
 
 def post_detail(request, year, month, day, post):
