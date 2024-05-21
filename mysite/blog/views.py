e9b4c37d7887e5360325d9f4566cc2d717b9ee40
santosh narayanan <santosh33@gmail.com>
2024-05-21 17:11:37 +0200

    Pagination working

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,19 +5,21 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
     # Pagination with 3 Post per page
     paginator = Paginator(post_list, 3)
-    page_number = request.GET.get('page')
-    posts = paginator.get_page(page_number)
-    return render(request, 'blog/post/list.html', {'posts': posts})
+    page_number = request.GET.get("page", 1)
+    posts = paginator.page(page_number)
+    return render(request, "blog/post/list.html", {"posts": posts})
 
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, 
-                             slug=post, 
-                             publish__year=year, 
-                             publish__month=month, 
-                             publish__day=day, 
-                             status=Post.Status.PUBLISHED)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    post = get_object_or_404(
+        Post,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
+        status=Post.Status.PUBLISHED,
+    )
+    return render(request, "blog/post/detail.html", {"post": post})
