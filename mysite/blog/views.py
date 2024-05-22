6f7750e3b43e894f5703f592619da96b16d7d268
santosh narayanan <santosh33@gmail.com>
2024-05-22 17:12:35 +0200

    sending emails in views-displaying form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,31 +4,60 @@ from .forms import EmailPostForm
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
+
 class PostListView(ListView):
     queryset = Post.published.all()
     context_object_name = "posts"
     paginate_by = 3
     template_name = "blog/post/list.html"
 
-def post_share(request,post_id):
-    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
     sent = False
-    if request.method == "POST":
-        # form was submitted
+
+    if request.method == 'POST':
+        # Form was submitted
         form = EmailPostForm(request.POST)
         if form.is_valid():
-            # form fields passed validation
+            # Form fields passed validation
             cd = form.cleaned_data
-            post_url = request.build_absolute_uri(post.get_absolute_url())
-            subject = (f"{cd['name']} recommends you read " f"{post.title}")
-            message = (f"Read {post.title} at {post_url}\n\n")
-            message += (f"{cd['name']}\'s comments: {cd['comments']}")
-            send_mail(subject, message, "nS9fM@example.com", [cd["to"]])
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd['to']],
+            )
             sent = True
-        else:
-            form = EmailPostForm()
-    return render(request, "blog/post/share.html", 
-                  {"post": post,"form": form, "sent": sent})
+
+    else:
+        form = EmailPostForm()
+    return render(
+        request,
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form,
+            'sent': sent
+        },
+    )
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,7 +126,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email Server Configuration
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
