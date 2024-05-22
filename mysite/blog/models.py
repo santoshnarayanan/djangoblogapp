e9ea9a31980831071258576de6b82180a995e95c
santosh narayanan <santosh33@gmail.com>
2024-05-22 18:08:07 +0200

    Comments Model migration applied

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,23 @@ from django.db import models
 from django.urls import reverse
 from django.utils import timezone
 
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes =[
+            models.Index(fields=['created']),
+        ]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
