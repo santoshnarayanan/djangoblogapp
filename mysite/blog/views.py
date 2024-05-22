775620ec9f7278fdb42e773e24769078ce9c9ad2
santosh narayanan <santosh33@gmail.com>
2024-05-22 18:42:39 +0200

    handle modelforms in views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from django.core.mail import send_mail
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from django.shortcuts import get_object_or_404, render
+from django.views.decorators.http import require_POST
 from .models import Post
 
 
@@ -69,3 +70,31 @@ def post_detail(request, year, month, day, post):
         status=Post.Status.PUBLISHED,
     )
     return render(request, "blog/post/detail.html", {"post": post})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED
+    )
+    comment = None
+    # A comment was posted
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # Create a Comment object without saving it to the database
+        comment = form.save(commit=False)
+        # Assign the current post to the comment
+        comment.post = post
+        # Save the comment to the database
+        comment.save()
+    return render(
+        request,
+        'blog/post/comment.html',
+        {
+            'post': post,
+            'form': form,
+            'comment': comment
+        }
+    )
