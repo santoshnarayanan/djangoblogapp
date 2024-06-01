bcfb03c861d07c5cbf22c411f27c20dda132ad8c
Santosh Narayanan <santosh33@gmail.com>
2024-06-01 17:12:33 +0200

    Reterive blogs by tags

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,36 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.shortcuts import get_object_or_404, render
 from django.views.decorators.http import require_POST
+from django.core.paginator import Paginator
+from taggit.models import Tag
 from .models import Post
+
+
+def post_list(request, tag_slug=None):
+    post_list = Post.published.all()
+    tag = None
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        post_list = post_list.filter(tags__in=[tag])
+    # Pagination with 3 posts per page
+    paginator = Paginator(post_list, 3)
+    page_number = request.GET.get('page',1)
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    return render(
+        request,
+        'blog/post/list.html',
+        {
+            'posts': posts,
+            'tag': tag
+        }
+    )
 
 
 class PostListView(ListView):
@@ -98,3 +127,5 @@ def post_comment(request, post_id):
             'comment': comment
         }
     )
+
+    
