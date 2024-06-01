706333e68f47c2470dbb26fbd46e101813331655
Santosh Narayanan <santosh33@gmail.com>
2024-06-01 23:42:14 +0200

    Displays latest post on sidebar

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -9,3 +9,13 @@ creating a simple tag to retrieve the total posts that have been published on th
 @register.simple_tag
 def total_posts():
     return Post.published.count()
+
+"""
+create another tag to display the latest posts in the sidebar of the blog
+Inclusion tags have to return a dictionary of values, which is used as the 
+context to render the specified template
+"""
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
